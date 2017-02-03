621807fbd0bc36b55b7351121eda99412443eb79
freightrain20 <freightrain20@gmail.com>
2017-02-02 20:51:52 -0800

    Add files via upload

--- video_pipeline.py
+++ video_pipeline.py
@@ -12,6 +12,11 @@ dist_pickle = pickle.load( open( "camera_cal_pickle.p", "rb" ) )
 mtx = dist_pickle["mtx"]
 dist = dist_pickle["dist"]
 
+global left_curverad_prev
+global right_curverad_prev
+
+left_curverad_prev = 0
+right_curverad_prev = 0
 # Unwarp the image
 def unwarp(img, mtx, dist):
     
@@ -128,6 +133,8 @@ def process_image(img):
     global x_val_L_prev
     global y_val_R_prev
     global x_val_R_prev
+    global left_curverad_prev
+    global right_curverad_prev
     
     # Yellow and white HLS thresholds
     max_thresh_y = (50, 190, 255)
@@ -136,8 +143,10 @@ def process_image(img):
     min_thresh_w = (0, 200, 0)
     
     # Unit conversion from pixels to meters
-    ym_per_pix = 3/150 # meters per pixel in y dimension
-    xm_per_pix = 3.7/850 # meteres per pixel in x dimension
+    #ym_per_pix = 3/150 # meters per pixel in y dimension
+    #xm_per_pix = 3.7/850 # meteres per pixel in x dimension
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
     
     # Main pipeline process
     # 1. Change image to overhead perspective
@@ -180,101 +189,94 @@ def process_image(img):
     # 7. Moving histogram window to identify left and right lane lines
     # Each histogram consists of half of the image space, eight histograms in total
     # The max value in the histogram on the left half and right half of the image are assumed to be lane lines
-    img_mask = np.zeros_like(combined)
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(combined[combined.shape[0]/2:,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((combined, combined, combined))*255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-    for i in range(9):
-        histogram = np.sum(combined[combined.shape[0]/2 - i*combined.shape[0]/16:combined.shape[0]-(i*combined.shape[0]/16),:], axis=0)
-        lane_L = histogram[:histogram.shape[0]/2].argmax()
-        lane_R = histogram[histogram.shape[0]/2:].argmax() + histogram.shape[0]/2
-        
-        # If lane location is too far from adjacent lane location, assume the value is incorrect and use adjacent value instead
-        if i > 0:
-            if (lane_L > lane_L_prev + 150) | (lane_L < lane_L_prev - 150):
-                lane_L = lane_L_prev
-            if (lane_R > lane_R_prev + 150) | (lane_R < lane_R_prev - 150):
-                lane_R = lane_R_prev
-        # Store lane location for current histogram for comparison to adjacent histogram
-        lane_L_prev = lane_L
-        lane_R_prev = lane_R
-        
-        # Build lane mask around identified max histogram locations
-        mask = np.zeros_like(histogram)
-        mask[lane_L - 30 : lane_L + 30] = 1
-        mask[lane_R - 30 : lane_R + 30] = 1     
-        mask_array = np.tile(mask,(combined.shape[0]/2,1))
-        img_mask[combined.shape[0]/2 - i*combined.shape[0]/16:combined.shape[0]-(i*combined.shape[0]/16),:] += mask_array
-        img_mask[img_mask > 0] = 1
-        
-    # 8. Fit a polynomial to estimated left and right lanes
-    # Add max histogram mask to combined filter
-    combined_masked = np.zeros_like(combined)
-    combined_masked[(combined == 1) & (img_mask == 1)] = 1
-    
-    # Define arrays for (x,y) coordinate of estimated left and right lanes
-    y_val_L = []
-    x_val_L = []
-    y_val_R = []
-    x_val_R = []
-    
-    # Determine if lane coordinate is part of left or right lane
-    for i in range(combined_masked.shape[0]):
-        for j in range(combined_masked.shape[1]):
-            if combined_masked[i,j] == 1:
-                if j < (combined_masked.shape[1] / 2):
-                    y_val_L.append(i)
-                    x_val_L.append(j)
-                else:
-                    y_val_R.append(i)
-                    x_val_R.append(j)  
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(combined.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = combined.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
 
-    y_val_L = np.array(y_val_L)
-    x_val_L = np.array(x_val_L)
-    y_val_R = np.array(y_val_R)
-    x_val_R = np.array(x_val_R)
-    
-    # If no lane is identified, use lane from previous image
-    if not y_val_L.size:
-        y_val_L = y_val_L_prev
-        x_val_L = x_val_L_prev
-    if not x_val_L.size:
-        y_val_L = y_val_L_prev
-        x_val_L = x_val_L_prev
-    if not y_val_R.size:
-        y_val_R = y_val_R_prev
-        x_val_R = x_val_R_prev
-    if not x_val_R.size:
-        y_val_R = y_val_R_prev
-        x_val_R = x_val_R_prev
-    y_val_L_prev = y_val_L
-    x_val_L_prev = x_val_L
-    y_val_R_prev = y_val_R
-    x_val_R_prev = x_val_R
-    
-    # Fit 2nd order polynomials to left lane coordinates
-    left_fit = np.polyfit(y_val_L, x_val_L, 2)
-    # Define a top and bottom limit to help with plotting
-    y_val_L = y_val_L[y_val_L > 300]
-    y_val_L = np.append(300, y_val_L)
-    y_val_L = np.append(y_val_L, top_down.shape[0])
-    left_fitx = left_fit[0]*y_val_L**2 + left_fit[1]*y_val_L + left_fit[2]
-    
-    # Fit 2nd order polynomials to right lane coordinates
-    right_fit = np.polyfit(y_val_R, x_val_R, 2)
-    # Define a top and bottom limit to help with plotting
-    y_val_R = y_val_R[y_val_R > 300]
-    y_val_R = np.append(300, y_val_R)
-    y_val_R = np.append(y_val_R, top_down.shape[0])
-    right_fitx = right_fit[0]*y_val_R**2 + right_fit[1]*y_val_R + right_fit[2]
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = combined.shape[0] - (window+1)*window_height
+        win_y_high = combined.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, combined.shape[0]-1, combined.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
     
     # 9. Calculate a radius of curvature by fitting a 2nd order polynomial half way between the left and right lane polynomials
-    y_val_temp = np.array([300, 450, top_down.shape[0]])
-    left_fitx_temp = left_fit[0]*y_val_temp**2 + left_fit[1]*y_val_temp + left_fit[2]
-    right_fitx_temp = right_fit[0]*y_val_temp**2 + right_fit[1]*y_val_temp + right_fit[2]
-    mid_fitx_temp = (left_fitx_temp + right_fitx_temp) / 2
-    mid_fit = np.polyfit(y_val_temp * ym_per_pix, mid_fitx_temp * xm_per_pix, 2)
+    # Fit new polynomials to x,y in world space
+    y_eval = np.max(ploty)
+    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
     
-    # Radius of curvature calc
-    rad_of_curve = round((1 + (2 * mid_fit[0] * top_down.shape[0] + mid_fit[1]) ** 2) ** (3/2) / abs(2 * mid_fit[0]), 1)
+    if left_curverad_prev:
+        left_curverad = round(0.1 * left_curverad + 0.9 * left_curverad_prev)
+        right_curverad = round(0.1 * right_curverad + 0.9 * right_curverad_prev)
+    
+    left_curverad_prev = left_curverad
+    right_curverad_prev = right_curverad
     
     # 10. Calculate the vehicle position within the lane
     # 530 pixels is the location of the center of the image, warped
@@ -283,12 +285,12 @@ def process_image(img):
     center_offset = round((center_of_lane - (left_fitx[-1] * xm_per_pix + 3.7 / 2)), 1)
 
     # Create an image to draw the lines on
-    warp_zero = np.zeros_like(combined_masked).astype(np.uint8)
+    warp_zero = np.zeros_like(combined).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
     
     # Recast the x and y points into usable format for cv2.fillPoly()
-    pts_left = np.array([np.transpose(np.vstack([left_fitx, y_val_L]))])
-    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, y_val_R])))])
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
     pts = np.hstack((pts_left, pts_right))
 
     # Draw the lane onto the warped blank image
@@ -300,7 +302,7 @@ def process_image(img):
     result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
     
     # Write radius of curvature and lane location to image
-    cv2.putText(result, 'Radius of Curvature: ' + str(rad_of_curve) + ' m', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 0), 3)
+    cv2.putText(result, 'Radius of Curvature: ' + str(left_curverad) + ' m (left) ' + str(right_curverad) + ' m (right)', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 0), 3)
     if center_offset >= 0:
         cv2.putText(result, 'Distance from Center of Lane: ' + str(center_offset) + ' m to the right', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 0), 3)
     else:
@@ -310,7 +312,7 @@ def process_image(img):
     return result
 
 # Video processing
-white_output = 'project_video_labeled_saturated_2.mp4'
+white_output = 'project_video_labeled_v3.mp4'
 clip1 = VideoFileClip("project_video.mp4")
 white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
 white_clip.write_videofile(white_output, audio=False)
